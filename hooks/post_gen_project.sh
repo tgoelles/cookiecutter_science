bf30cee0abbda0674c857f24da0d12516da02e20
thomasgoelles <thomas.goelles@gmail.com>
2021-11-30 11:38:07 +0100

    testing better post_gen

--- hooks/post_gen_project.sh
+++ hooks/post_gen_project.sh
@@ -3,17 +3,22 @@
 # organisation qeustion
 # create a repository in an organization
 #  $ gh repo create cli/my-project
-read -p "Create a github repo? [y/n] " -n 1 -r
-echo
-if [[ $REPLY =~ ^[Yy]$ ]]
-then
-    git init
-    git add .
-    git commit -m "initial commit"
-    gh repo create {{ cookiecutter.repo_name }} --description {{ cookiecutter.description }}
-    git push --set-upstream origin master
 
-fi
+{% if cookiecutter.generate_git_repo == 'y' -%}
+echo "git init"
+git init
+git add .
+git commit -m "initial commit"
+{%- if cookiecutter.github_organisation == "1" -%}
+echo "init personal repo"
+gh repo create {{ cookiecutter.repo_name }} --description {{ cookiecutter.description }}
+{% else %}
+echo "init organisation repo"
+gh repo create {{ cookiecutter.github_organisation }}/{{ cookiecutter.repo_name }} --description {{ cookiecutter.description }}
+{% endif %}
+git push --set-upstream origin master
+{% endif %}
+
 
 echo "Done."
 echo "Now open a new window of VS Code and open the folder {{ cookiecutter.repo_name }} "
