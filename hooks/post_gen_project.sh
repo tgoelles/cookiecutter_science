d0939eb6bd5efddc5467ad61e5d1b4201503ce5b
thomasgoelles <thomas.goelles@gmail.com>
2021-12-01 11:18:09 +0100

    asking vor python version

--- hooks/post_gen_project.sh
+++ hooks/post_gen_project.sh
@@ -10,10 +10,13 @@ git init
 git add .
 git commit -m "initial commit"
 echo "init repo at {{ cookiecutter.github_organisation }}"
-gh repo create {{ cookiecutter.github_organisation }}/{{ cookiecutter.repo_name }} --description {{ cookiecutter.description }}
+gh && gh repo create {{ cookiecutter.github_organisation }}/{{ cookiecutter.repo_name }} --description {{ cookiecutter.description }} || echo "github CLI gh not installed"
 git push --set-upstream origin master
 {% endif %}
 
+cd {{ cookiecutter.repo_name }}
+
+code && code -n . || echo "code command not found"
 
 echo "Done."
 echo "Now open a new window of VS Code and open the folder {{ cookiecutter.repo_name }} "
